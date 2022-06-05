85f8549c931db41f3de2bf4e014f1a912c7a3a77
mohin <mohinheikh2011@gmail.com>
2022-06-05 12:44:51 +0530

    changes in schema

--- src/models/user.schema.ts
+++ src/models/user.schema.ts
@@ -1,16 +1,36 @@
 import * as mongoose from 'mongoose';
 import * as bcrypt from 'bcrypt';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
-export const UserSchema = new mongoose.Schema({
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  name: { type: String, required: true },
-  phoneNumber: { type: String, unique: true, required: true },
-  age: { type: Number, required: true },
-  gender: { type: String, required: true },
-  idDeleted: { type: Boolean, default: false },
-  role: { type: String, required: true },
-})
+@Schema()
+export class User {
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  phoneNumber: string;
+
+  @Prop()
+  age: number;
+
+  @Prop()
+  gender: string;
+
+  @Prop()
+  isDeleted: boolean;
+
+  @Prop()
+  role: string;
+}
+
+
+export const UserSchema = SchemaFactory.createForClass(User);
 
 UserSchema.pre('save', async function (next: mongoose.HookNextFunction) {
   try {
